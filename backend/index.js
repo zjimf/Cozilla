bbc0a955184968ffed0eb995a31ed70e4787db6d
Cysepz <cysepz@gmail.com>
2025-02-15 04:04:01 +0800

    conf: routes/index

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/transferCode", require("./routes/transferCode"));
-app.use("/buildEnv", require("./routes/buildEnv"));
+app.use("/deploy", require("./routes/deploy"));
 app.use("/generateYaml", require("./routes/generateYaml"));
-app.use("/deployCode", require("./routes/deployCode"));
 app.use("/testCode", require("./routes/testCode"));
 app.use("/retransferCode", require("./routes/retransferCode"));
 app.use("/fixCode", require("./routes/fixCode"));
